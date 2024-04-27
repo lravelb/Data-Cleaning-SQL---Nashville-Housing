bbdd3c57fbb5036c4de2c4e00c49a0ce5bc8ee5d
Luisina Ravel <149938522+lravelb@users.noreply.github.com>
2024-04-27 14:42:29 +0200

    Update DataCleaningProject_Nashville.sql

--- DataCleaningProject_Nashville.sql
+++ DataCleaningProject_Nashville.sql
@@ -1,6 +1,6 @@
 -- Data cleaning project
 
--- Let´s take a look to the table and verify the data to clean, modify and/or standarize
+-- LetÂ´s take a look to the table and verify the data to clean, modify and/or standarize
 
 SELECT *
 FROM DataCleaning_Nashville..NashvilleHousing
@@ -15,7 +15,7 @@ UPDATE 	NashvilleHousing
 SET SaleDateConverted = CONVERT(DATE, saledate)
 
 
--- Property Address: check the NULLs. Can any column be referenced? Yes, ParcelID. Let´s to complete the PropertyAddress
+-- Property Address: check the NULLs. Can any column be referenced? Yes, ParcelID. LetÂ´s to complete the PropertyAddress
 
 SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
 FROM DataCleaning_Nashville..NashvilleHousing a
@@ -37,7 +37,7 @@ WHERE a.PropertyAddress IS NULL
 
 SELECT
 SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) AS address,
---CHARINDEX(',', PropertyAddress) --con charindex cuento cuantos lugares hasta la coma, como no la quiero, por eso pongo en la funcion de arriba -1
+--CHARINDEX(',', PropertyAddress)
 SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) AS city
 FROM DataCleaning_Nashville..NashvilleHousing
 
@@ -57,8 +57,8 @@ SET CityProperty = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1
 -- OwnerAddress. Break into individual columns: Address, City, State
 
 SELECT
-PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),--parsename is useful for periods. Replace comas for periods (punto como simbolo en ingles)
-PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2), --si pongo 1 toma hasta la primera coma pero desde atras
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2),
 PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
 FROM DataCleaning_Nashville..NashvilleHousing
 
@@ -114,15 +114,11 @@ FROM DataCleaning_Nashville..NashvilleHousing
 )
 /*SELECT *
 FROM row_num
-WHERE row_num >1 -- si no hubiera hecho cte no podria hacer este where. es para saber cuantas lineas repiten los valores importantes mencionados en el cte
-
---1RO lo hago para consultar, luego cambio el select * por delete y luego select otra vez para verificar la si se borro
+WHERE row_num >1 */
 DELETE
 FROM row_num
 WHERE row_num > 1 */
-SELECT *
-FROM row_num
-WHERE row_num >1
+
 
 -- Delete unused columns
 
